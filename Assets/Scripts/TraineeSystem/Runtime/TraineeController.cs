831e135ee3c1a62cb0a3c399929aeb565d110b27
PostMail-hub <gmlals2949@naver.com>
2025-06-26 20:32:20 +0900

    feat: 10연 뽑기 시스템 구현 및 스킵 기능 추가
    - 10연 뽑기 연출은 수정 예정

--- Assets/Scripts/TraineeSystem/Runtime/TraineeController.cs
+++ Assets/Scripts/TraineeSystem/Runtime/TraineeController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class TraineeController : MonoBehaviour
 {
@@ -8,16 +10,37 @@ public class TraineeController : MonoBehaviour
     [SerializeField] private TraineeCardUI cardUI;
     [SerializeField] private TraineeCardAnimator cardAnimator;
 
+    [SerializeField] private TMP_Text orderText;
+    [SerializeField] private Button skipButton;
+
     private bool isFlipped = false;
     private bool isFlipping = false;
+    private int currentIndex = 0;
 
-    public void Setup(TraineeData traineeData, TraineeFactory traineeFactory)
+    private System.Action<int> onSkip;
+    private System.Action<TraineeData> onConfirm;
+
+    public void Setup(TraineeData traineeData, TraineeFactory traineeFactory, int order,
+                      System.Action<int> onSkipAction, System.Action<TraineeData> onConfirmAction)
     {
         data = traineeData;
         factory = traineeFactory;
         isFlipped = false;
+        currentIndex = order;
+        onSkip = onSkipAction;
+        onConfirm = onConfirmAction;
 
         cardUI?.UpdateUI(data);
+
+        if (orderText != null)
+            orderText.text = $"{currentIndex + 1}번째 카드";
+
+        if (skipButton != null)
+        {
+            skipButton.gameObject.SetActive(true);
+            skipButton.onClick.RemoveAllListeners();
+            skipButton.onClick.AddListener(() => onSkip?.Invoke(currentIndex));
+        }
     }
 
     public void OnClick_DeleteSelf()
@@ -27,6 +50,7 @@ public class TraineeController : MonoBehaviour
 
     public void OnClick_FrontCard()
     {
+        onConfirm?.Invoke(data);
         Destroy(gameObject);
         factory?.SetCanRecruit(true);
     }
