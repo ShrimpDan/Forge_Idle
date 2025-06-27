61f82901c2f07f6a0088d7020041370a5ecce6c2
PKT <rudxorrmrwkd@gmail.com>
2025-06-27 11:54:45 +0900

    feat : MiniGame 버튼 연동

---
+++ Assets/Scripts/UI/WindowUI/MineMiniGameWindow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MineMiniGameWindow : MonoBehaviour
+{
+    public void OnClickMiniGame()
+    {
+        SceneManager.LoadScene("MiniGame", LoadSceneMode.Additive);
+    }
+
+ 
+}
