320d4168dea08604628023f4be61a2cc5b04362e
PostMail-hub <gmlals2949@naver.com>
2025-07-04 20:45:30 +0900

    feat: 제자 데이터 JSON 기반 연동 시스템 완성 + ScriptableObjects 삭제

--- Assets/Scripts/TraineeSystem/UI/Icons/FusionSlotView.cs
+++ Assets/Scripts/TraineeSystem/UI/Icons/FusionSlotView.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
+/// <summary>
+/// 합성 슬롯 하나를 구성하고, 클릭 시 해당 제자를 선택 또는 제거할 수 있도록 함.
+/// </summary>
 public class FusionSlotView : MonoBehaviour
 {
     [SerializeField] private TMP_Text nameText;
     [SerializeField] private TMP_Text specializationText;
 
     private TraineeData currentData;
-    private Action onClicked;
+    private System.Action onClicked;
 
     public TraineeData Data => currentData;
-    public Action OnClick => onClicked;
+    public System.Action OnClick => onClicked;
 
     private void Awake()
     {
@@ -27,7 +30,7 @@ public class FusionSlotView : MonoBehaviour
     /// <summary>
     /// 제자 데이터를 슬롯에 세팅하고, 클릭 시 호출할 콜백을 등록
     /// </summary>
-    public void SetData(TraineeData data, Action onClicked = null)
+    public void SetData(TraineeData data, System.Action onClicked = null)
     {
         currentData = data;
         this.onClicked = onClicked;
@@ -36,12 +39,11 @@ public class FusionSlotView : MonoBehaviour
         {
             nameText.text = "비어 있음";
             specializationText.text = "";
+            return;
         }
-        else
-        {
-            nameText.text = data.Name;
-            specializationText.text = GetKoreanSpecialization(data.Specialization);
-        }
+
+        nameText.text = data.Name;
+        specializationText.text = GetKoreanSpecialization(data.Specialization);
     }
 
     /// <summary>
@@ -60,7 +62,10 @@ public class FusionSlotView : MonoBehaviour
         SetData(null, null);
     }
 
-    private string GetKoreanSpecialization(SpecializationType type)
+    /// <summary>
+    /// 특화 타입을 한글로 반환
+    /// </summary>
+    public static string GetKoreanSpecialization(SpecializationType type)
     {
         return type switch
         {
