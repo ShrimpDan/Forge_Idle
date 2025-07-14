45402f913bd375dc76d257b95b5176ceb7aa11b8
leesoomang <soo199617@gmail.com>
2025-07-14 12:29:11 +0900

    feat : 마인씬 어시스턴트 슬롯 생성

--- Assets/Scripts/Manager/LoadSceneManager.cs
+++ Assets/Scripts/Manager/LoadSceneManager.cs
@@ -52,18 +52,24 @@ public class LoadSceneManager : MonoSingleton<LoadSceneManager>
         StartCoroutine(LoadSceneCoroutine(SceneName.GetSceneByType(type), isAdditve));
     }
 
-    IEnumerator LoadSceneCoroutine(string sceneName, bool isAdditve)
+    IEnumerator LoadSceneCoroutine(string sceneName, bool isAdditive)
     {
-        // 로딩 스크린 페이드 아웃
         yield return StartCoroutine(FadeRoutine(fadeOutCurve, true));
 
-        // 비동기 씬 로딩
-        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName, isAdditve ? LoadSceneMode.Additive : LoadSceneMode.Single);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName, isAdditive ? LoadSceneMode.Additive : LoadSceneMode.Single);
         yield return new WaitUntil(() => asyncOperation.isDone);
 
-        // 로딩 스크린 페이드 인
+        // 씬 최상위로
+        if (isAdditive)
+        {
+            Scene loadedScene = SceneManager.GetSceneByName(sceneName);
+            if (loadedScene.IsValid())
+                SceneManager.SetActiveScene(loadedScene);
+        }
+
         yield return StartCoroutine(FadeRoutine(fadeInCurve, false));
     }
+
 
     public void UnLoadScene(SceneType type)
     {
--- Assets/Scripts/Mine/MineSceneManager.cs
+++ Assets/Scripts/Mine/MineSceneManager.cs
@@ -2,13 +2,13 @@
 
 public class MineSceneManager : MonoBehaviour
 {
-    [Header("메인 오브젝트")]
+    [Header("메인 오브젝트 (상세맵)")]
     public GameObject mineDetailMap;
 
-    [Header("마인 프리팹들")]
+    [Header("마인 프리팹들 (순서 맞게)")]
     public GameObject[] minePrefabs; // 0=Copper, 1=Iron, 2=Gold, 3=Gem
 
-    [Header("카메라 리밋")]
+    [Header("카메라 리밋 (0=디테일맵, 1~4=각 마인)")]
     public CameraLimit[] cameraLimits; // [0]=DetailMap, [1]=Copper, [2]=Iron, [3]=Gold, [4]=Gem
 
     [Header("카메라 이동 컴포넌트")]
@@ -16,31 +16,65 @@ public class MineSceneManager : MonoBehaviour
 
     private int currentMineIdx = 0;
 
+    private GameObject mainSceneCam;
+    private GameObject mineSceneCam;
+
     private void Start()
     {
         ShowMineDetailMap();
     }
 
+    private void OnEnable()
+    {
+        mainSceneCam = GameObject.Find("MainCamera");
+        if (mainSceneCam != null) mainSceneCam.SetActive(false);
+
+        mineSceneCam = GameObject.Find("MapCamera");
+        if (mineSceneCam != null) mineSceneCam.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        if (mainSceneCam != null) mainSceneCam.SetActive(true);
+        if (mineSceneCam != null) mineSceneCam.SetActive(false);
+    }
+
+    // 모든 맵, 마인 프리팹 비활성화
+    private void SetAllInactive()
+    {
+        if (mineDetailMap != null)
+            mineDetailMap.SetActive(false);
+
+        if (minePrefabs != null)
+        {
+            foreach (var prefab in minePrefabs)
+                if (prefab != null)
+                    prefab.SetActive(false);
+        }
+    }
+
+    // 상세맵 보기 (마인 프리팹 전부 끔)
     public void ShowMineDetailMap()
     {
-        mineDetailMap.SetActive(true);
-        for (int i = 0; i < minePrefabs.Length; i++)
-            minePrefabs[i].SetActive(false);
+        SetAllInactive();
+        if (mineDetailMap != null)
+            mineDetailMap.SetActive(true);
 
         if (cameraTouchDrag != null && cameraLimits.Length > 0)
             cameraTouchDrag.SetCameraLimit(cameraLimits[0]);
+
         currentMineIdx = 0;
     }
 
     public void ShowMine(int mineIndex)
     {
-        mineDetailMap.SetActive(false);
-        for (int i = 0; i < minePrefabs.Length; i++)
-            minePrefabs[i].SetActive(i == mineIndex);
+        SetAllInactive();
+        if (minePrefabs != null && mineIndex >= 0 && mineIndex < minePrefabs.Length)
+            minePrefabs[mineIndex].SetActive(true);
 
-        // cameraLimits[1] = 0번 마인, [2] = 1번 마인...
         if (cameraTouchDrag != null && cameraLimits.Length > mineIndex + 1)
             cameraTouchDrag.SetCameraLimit(cameraLimits[mineIndex + 1]);
+
         currentMineIdx = mineIndex + 1;
     }
 
@@ -52,6 +86,6 @@ public class MineSceneManager : MonoBehaviour
 
     public void OnGoToMainScene()
     {
-        LoadSceneManager.Instance.LoadSceneAsync(SceneType.Main);
+        LoadSceneManager.Instance.UnLoadScene(SceneType.MineScene);
     }
 }
