413e1424e6411a2a698a662c14179181f1a392a3
PostMail-hub <gmlals2949@naver.com>
2025-07-23 13:08:28 +0900

    feat: 제자 특화 별 영입 추가

--- Assets/Scripts/UI/Held/RecruitButtonHandler.cs
+++ Assets/Scripts/UI/Held/RecruitButtonHandler.cs
@@ -4,7 +4,10 @@ using UnityEngine.UI;
 public class RecruitButtonHandler : MonoBehaviour
 {
     [Header("버튼")]
-    [SerializeField] private Button btnRecruitOne;
+    [SerializeField] private Button btnRecruitRandom;
+    [SerializeField] private Button btnRecruitCrafting;
+    [SerializeField] private Button btnRecruitSelling;
+    [SerializeField] private Button btnRecruitMining;
     [SerializeField] private Button btnApprove;
     [SerializeField] private Button btnReject;
     [SerializeField] private Button btnHold;
@@ -14,33 +17,28 @@ public class RecruitButtonHandler : MonoBehaviour
 
     private void Awake()
     {
-        btnRecruitOne.onClick.AddListener(OnClickRecruitOne);
+        btnRecruitRandom.onClick.AddListener(() => OnClickRecruitByType(null));
+        btnRecruitCrafting.onClick.AddListener(() => OnClickRecruitByType(SpecializationType.Crafting));
+        btnRecruitSelling.onClick.AddListener(() => OnClickRecruitByType(SpecializationType.Selling));
+        btnRecruitMining.onClick.AddListener(() => OnClickRecruitByType(SpecializationType.Mining));
+
         btnApprove.onClick.AddListener(OnClickApprove);
         btnReject.onClick.AddListener(OnClickReject);
         btnHold.onClick.AddListener(OnClickHold);
     }
 
-    // 제자 1명 뽑기
-    public void OnClickRecruitOne()
+    // 선택한 특화에 따라 제자 뽑기
+    private void OnClickRecruitByType(SpecializationType? type)
     {
-        previewManager.TryRecruitCandidate();
+        previewManager.TryRecruitCandidateByType(type);
     }
 
     // 영입 확정
-    public void OnClickApprove()
-    {
-        previewManager.ApproveCandidate();
-    }
+    public void OnClickApprove() => previewManager.ApproveCandidate();
 
     // 영입 거절
-    public void OnClickReject()
-    {
-        previewManager.RejectCandidate();
-    }
+    public void OnClickReject() => previewManager.RejectCandidate();
 
     // 보류 처리
-    public void OnClickHold()
-    {
-        previewManager.HoldCandidate();
-    }
+    public void OnClickHold() => previewManager.HoldCandidate();
 }
