b02bdba4854cffccac78dda62e0045c280f08073
leesoomang <soo199617@gmail.com>
2025-08-05 14:59:51 +0900

    feat : 타이틀씬 BGM 추가

--- Assets/Scripts/Manager/TitleSceneManager.cs
+++ Assets/Scripts/Manager/TitleSceneManager.cs
@@ -6,14 +6,19 @@ public class TitleSceneManager : MonoBehaviour
     [SerializeField] Animator titleAnim;
     [SerializeField] Button startButton;
 
+    private SoundManager soundManager;
+
     private void Awake()
     {
+        soundManager = SoundManager.Instance;
         Application.targetFrameRate = 60;
         startButton.onClick.AddListener(ClickStartBtn);
     }
 
     private void Start()
     {
+        SoundManager.Instance?.Play("MainBGM");
+
         if (titleAnim != null)
             titleAnim.SetTrigger("Start");
     }
